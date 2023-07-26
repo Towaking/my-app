8b6bafd4e7bc257ed9ff5f1e7188c95c10f1a261
Towaking <goldwinho@gmail.com>
2023-07-26 15:17:37 +0700

    switch login with register

--- src/component/register.jsx
+++ src/component/register.jsx
@@ -1,5 +1,33 @@
 export const Register = () => {
     return (
-        <>register</>
+        <>
+            <form>
+                <div>
+                    <div className="fname">
+                        <label htmlFor="fname">First Name</label>
+                        <input type="text" id="fname" name="fname" />
+                    </div>
+                    <div className="lname">
+                        <label htmlFor="lname">Last Name</label>
+                        <input type="text" id="lname" name="lname" />
+                    </div>
+                </div>
+                <div className="name">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" id="email" name="email" />
+                </div>
+                <div className="pw">
+                    <label htmlFor="pw">Password</label>
+                    <input type="password" id="pw" name="pw" />
+                </div>
+                <div className="stay">
+                    <input type="checkbox" name="sty" id="sty" />
+                    <label htmlFor="sty">Stay sign in</label>
+                </div>
+                <div className="button">
+                    <button>Create</button>
+                </div>
+            </form>
+        </>
     );
 }
