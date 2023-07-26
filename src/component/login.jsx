c470fb0cafc27dad09bf8a43c502ec415675ff3b
Towaking <goldwinho@gmail.com>
2023-07-26 16:33:46 +0700

    part 2

--- src/component/login.jsx
+++ src/component/login.jsx
@@ -1,8 +1,38 @@
 export const Login = () => {
     return (
-        <>
-            login
-        </>
+        <div className="form">
+            <div className="vector"></div>
+            <div className="right">
+                <label className="title">Create Account</label>
+                <form>
+                    <div className="name">
+                        <div className="fname">
+                            <label htmlFor="fname">First Name</label>
+                            <input type="text" id="fname" name="fname" className="input" />
+                        </div>
+                        <div className="lname">
+                            <label htmlFor="lname">Last Name</label>
+                            <input type="text" id="lname" name="lname" className="input"/>
+                        </div>
+                    </div>
+                    <div className="email">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" name="email" className="input"/>
+                    </div>
+                    <div className="pw">
+                        <label htmlFor="pw">Password</label>
+                        <input type="password" id="pw" name="pw" className="input"/>
+                    </div>
+                    <div className="stay">
+                        <input type="checkbox" name="sty" id="sty"  />
+                        <label htmlFor="sty" id="stas" >Stay sign in</label>
+                    </div>
+                    <div className="button">
+                        <button>Create</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 
 }
--- src/component/register.jsx
+++ src/component/register.jsx
@@ -1,33 +1,38 @@
 export const Register = () => {
+
     return (
-        <>
-            <form>
-                <div>
-                    <div className="fname">
-                        <label htmlFor="fname">First Name</label>
-                        <input type="text" id="fname" name="fname" />
+        <div className="form">
+            <div className="vector"></div>
+            <div className="right">
+                <label className="title">Create Account</label>
+                <form>
+                    <div className="name">
+                        <div className="fname">
+                            <label htmlFor="fname">First Name</label>
+                            <input type="text" id="fname" name="fname" className="input" />
+                        </div>
+                        <div className="lname">
+                            <label htmlFor="lname">Last Name</label>
+                            <input type="text" id="lname" name="lname" className="input"/>
+                        </div>
                     </div>
-                    <div className="lname">
-                        <label htmlFor="lname">Last Name</label>
-                        <input type="text" id="lname" name="lname" />
+                    <div className="email">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" name="email" className="input"/>
                     </div>
-                </div>
-                <div className="name">
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" name="email" />
-                </div>
-                <div className="pw">
-                    <label htmlFor="pw">Password</label>
-                    <input type="password" id="pw" name="pw" />
-                </div>
-                <div className="stay">
-                    <input type="checkbox" name="sty" id="sty" />
-                    <label htmlFor="sty">Stay sign in</label>
-                </div>
-                <div className="button">
-                    <button>Create</button>
-                </div>
-            </form>
-        </>
+                    <div className="pw">
+                        <label htmlFor="pw">Password</label>
+                        <input type="password" id="pw" name="pw" className="input"/>
+                    </div>
+                    <div className="stay">
+                        <input type="checkbox" name="sty" id="sty"  />
+                        <label htmlFor="sty" id="stas" >Stay sign in</label>
+                    </div>
+                    <div className="button">
+                        <button>Create</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 }
